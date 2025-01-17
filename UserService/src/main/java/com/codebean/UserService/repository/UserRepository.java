498599c77cec409c36fa759c22cf87b6235cac8c
Mu'ti C Putro <mcpoetro@gmail.com>
2025-01-17 18:21:43 +0700

    UserService: add repository

--- UserService/src/main/java/com/codebean/UserService/repository/UserRepository.java
+++ UserService/src/main/java/com/codebean/UserService/repository/UserRepository.java
@@ -14,6 +14,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
@@ -22,4 +23,6 @@ public interface UserRepository extends CrudRepository<User, Long> {
     boolean existsByUsername(String username);
 
     List<User> findAllByRole_Name(String role);
+
+    Optional<User> findFirstByUsername(String username);
 }
