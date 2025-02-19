96a036b51f8d824f8c3677698f773bdb60f3b132
Mu'ti C Putro <mcpoetro@gmail.com>
2025-02-19 16:44:45 +0700

    change from dbsession to redis session

--- WebsiteUI/src/main/java/com/codebean/websiteui/config/JdbcConfig.java
+++ WebsiteUI/src/main/java/com/codebean/websiteui/config/JdbcConfig.java
@@ -1,18 +1,18 @@
-package com.codebean.websiteui.config;
-
-/*
-IntelliJ IDEA 2024.2.4 (Community Edition)
-Build #IC-242.23726.103, built on October 23, 2024
-@Author mcputro a.k.a. Mu'ti Cahyono Putro
-Created on 14 Feb 2025 10:36
-@Last Modified 14 Feb 2025 10:36
-Version 1.0
-*/
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-
-@Configuration
-@EnableJdbcHttpSession
-public class JdbcConfig {
-}
+//package com.codebean.websiteui.config;
+//
+///*
+//IntelliJ IDEA 2024.2.4 (Community Edition)
+//Build #IC-242.23726.103, built on October 23, 2024
+//@Author mcputro a.k.a. Mu'ti Cahyono Putro
+//Created on 14 Feb 2025 10:36
+//@Last Modified 14 Feb 2025 10:36
+//Version 1.0
+//*/
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+//
+//@Configuration
+//@EnableJdbcHttpSession
+//public class JdbcConfig {
+//}
